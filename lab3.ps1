9e281ec7284c8a9d3345dae1c387bbaf8fed449a
uc-hoffmac5 <hoffmac5@mail.uc.edu>
2019-09-22 23:42:56 -0400

    adding powershell files

---
+++ lab3.ps1
@@ -0,0 +1,10 @@
+﻿function getIP { (get-netipaddress).ipv4address | select-string "192.*" }
+$IP = getIP
+$Username= $env:Username
+$Hosnamet= $env:COMPUTERNAME
+$Version=$host.version
+$date=Get-Date
+$body=("This machine's IP is $IP. User is $Username. Hostname is $Hosnamet. Powershell version is $Version.
+Today's date is $date.")
+
+Send-MailMessage -To "hoffmac5@mail.uc.edu" -From "choffman0918@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $body -smtpserver smtp.gmail.com -port 587 -UseSSL -Credential(Get-Credential)
---
+++ sysinfo.ps1
@@ -0,0 +1,4 @@
+﻿function getIP { (get-netipaddress).ipv4address | select-string "192.*" }
+$IP = getIP
+Write-host("This machine's IP address is $IP")
+Write-host("This machine's IP address is {0}"-f $IP)
